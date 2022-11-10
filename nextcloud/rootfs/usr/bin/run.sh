95f0c08d3ddae9df8ae39377595d4f5538cf771a
Discomania <daniele.ponzo@libero.it>
2022-11-10 20:10:08 +0100

    error solving

--- nextcloud/rootfs/usr/bin/run.sh
+++ nextcloud/rootfs/usr/bin/run.sh
@@ -4,6 +4,8 @@ set -e
 SHARE_DIR=/share/nextcloud
 if [ ! -d "${SHARE_DIR}" ]; then
     mkdir -p "${SHARE_DIR}"
+    mkdir -p "${SHARE_DIR}/html"
+    mkdir -p "${SHARE_DIR}/data"
     chown -R www-data:root "${SHARE_DIR}"
     chmod -R g=u "${SHARE_DIR}"
 fi
